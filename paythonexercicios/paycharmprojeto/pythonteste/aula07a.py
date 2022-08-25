f13621e28fb84c89404748b75318d0e14f9f1bad
Lucas Ferreira Silva <silvaflucas88@gmail.com>
2022-08-25 01:00:16 -0300

    Create aula07a.py

---
+++ paythonexercicios/paycharmprojeto/pythonteste/aula07a.py
@@ -0,0 +1,10 @@
+n1 = int(input('deidite um valor?'))
+n2 = int(input('digite outro valor?'))
+
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+d = n1 // n2
+e = n1 ** n2
+print('soma e {}, o pronto e {} e a divisao e {:.3f}' . format(s, m, d), end='>>>> ')
+print('divisao inteira {} e potenciacao {}' . format('di', e))
