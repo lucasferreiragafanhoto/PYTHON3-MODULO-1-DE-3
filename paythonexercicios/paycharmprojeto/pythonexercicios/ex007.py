53955eff0b7b6b4c3b6761c0e12603b278f5fc7b
Lucas Ferreira Silva <silvaflucas88@gmail.com>
2022-08-26 06:15:10 -0300

    Create ex007.py

---
+++ paythonexercicios/paycharmprojeto/pythonexercicios/ex007.py
@@ -0,0 +1,4 @@
+n1 = float(input('primeira nota do aluno:'))
+n2 = float(input('segunda nota do aluno:'))
+media = (n1 + n2) / 2
+print('amendia entre {:.1f}  é igual a {:.1f}'.format(n1, n2, media))
