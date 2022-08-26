531c8c5501d9d638cf4f7a249f06ba73dc03789e
Lucas Ferreira Silva <silvaflucas88@gmail.com>
2022-08-25 23:55:41 -0300

    exercicio

---
+++ paythonexercicios/paycharmprojeto/pythonexercicios/ex005.py
@@ -0,0 +1,8 @@
+n = int(input('digite um numero:'))
+a = n - 1
+s = n + 2
+print('analisando o valor {} seu antecessor é {} é o sucessor {}' . format (n, a, s))
+
+print('esta e outra forma de fazer seu antecessor e o sucessor  ')
+n = int(input('digite um número:'))
+print(' analisando o valor {} seu antecessor é {} é o sucessor é {}' . format(n,  (n-1), (n+1)))
---
+++ paythonexercicios/paycharmprojeto/pythonexercicios/ex006.py
@@ -0,0 +1,21 @@
+n = int(input('digite um número?'))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('o dobro de {} vale {}.'.format (n, d))
+print('o triplo de {} vale {}.\na raiz quadrada de {} a {:.3f}.'.format(n, t,  n, r))
+
+print('temos mais duas formas de apresentar o exercicio!?')
+
+n = int(input('digite um número?'))
+print('o dobro de {} vale {}.'.format(n, (n*2)))
+print('o triplo de {} vale.{}. \na raiz quadrada de {} a {:.2f}.'.format(n, (n * 3), n, n**(1/2)))
+
+print('esta e o segunda modelo!')
+
+
+n = int(input('digite um numero'))
+print('o dobro de {} vale {}.'.format(n, (n*2)))
+print('o triplo de {} vale {}. \na raiz quadrada de {} a {:.2f}.'.format(n, (n*3), n, pow(n, 1/2)))
+
+
