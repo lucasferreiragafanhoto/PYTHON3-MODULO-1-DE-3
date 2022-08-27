8fc694b1e3657ff29b701f206a76087bcbfd4e38
Lucas Ferreira Silva <silvaflucas88@gmail.com>
2022-08-27 05:53:57 -0300

    Update ex013.py

--- paythonexercicios/paycharmprojeto/pythonexercicios/ex013.py
+++ paythonexercicios/paycharmprojeto/pythonexercicios/ex013.py
@@ -1,3 +1,3 @@
-salario = float(input('qual seu salario?R$'))
+salario = float(input('qual  seu salario?R$'))
 novo = salario + (salario * 15 / 100)
 print('o salario que era R$ {} com o novo reajuste de 15% o salario vai para R$ {}'. format(salario, novo))
